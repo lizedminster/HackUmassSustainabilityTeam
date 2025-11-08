1c2201c7c1336f741d3707a835910ee53dc2b293
Elizabeth Edminster <109642264+lizedminster@users.noreply.github.com>
2025-11-08 00:01:26 -0500

    Background removal success

--- testingBackgroundRemoval.py
+++ testingBackgroundRemoval.py
@@ -5,13 +5,16 @@ from PIL import Image
 input_path =  'exampleTrash.jpg'
 
 # Store path of the output image in the variable output_path
-output_path = 'exampleTrashBlank.jpg'
+output_path = 'exampleTrashBlank.png'
 
 # Processing the image
 input = Image.open(input_path)
 
 # Removing the background from the given Image
+print("123")
 output = remove(input)
 
 #Saving the image in the given path
+print("456")
 output.save(output_path)
+print("789")
